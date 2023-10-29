742bbf859e78bfda1a1148783a4bcd29151c01ae
PavithraN2004 <117561936+PavithraN2004@users.noreply.github.com>
2023-10-29 15:01:47 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleColor() {
-    var text = document.getElementById("demo");
-    if (text.style.color === "transparent") {
-      text.style.color = "black";
-    } else {
-      text.style.color = "transparent";
-    }
-}
